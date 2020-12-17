48324dbc2eeb1c61108c431e04aa0f0d09293e16
Renato Spakauskas <renatospaka@gmail.com>
2020-12-17 13:25:11 -0300

    Getting posts

--- web/main.go
+++ web/main.go
@@ -1,19 +1,20 @@
 package main
 
 import (
-	"html/template"
-	"fmt"
-	"net/http"
-	_"github.com/go-sql-driver/mysql"
 	"database/sql"
+	"fmt"
+	"html/template"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 	"github.com/gorilla/mux"
 )
 
 // Post é apenas um post
 type Post struct {
-	ID int
+	ID    int
 	Title string
-	Body string
+	Body  string
 }
 
 var db, err = sql.Open("mysql", "root:root@/go_course?charset=utf8")
@@ -23,17 +24,19 @@ func main() {
 	router := mux.NewRouter()
 	router.PathPrefix("/static").Handler(http.StripPrefix("/static", http.FileServer(http.Dir("static/"))))
 	router.HandleFunc("/", HomeHandler)
+	router.HandleFunc("/{id}/view", ViewHandler)
 
 	fmt.Println(http.ListenAndServe(":8080", router))
 }
 
-func checkErr (err error) {
+func checkErr(err error) {
 	if err != nil {
 		panic(err)
 	}
 }
 
-func listPosts() []Post {
+//GetPosts retrieves all posts
+func GetPosts() []Post {
 	rows, err := db.Query("select * from posts")
 	checkErr(err)
 
@@ -49,10 +52,27 @@ func listPosts() []Post {
 	return items
 }
 
+//GetPostById retrieves a specific post based on its id
+func GetPostById(id string) Post {
+	post := Post{}
+	row := db.QueryRow("select * from posts where id = ?", id)
+	row.Scan(&post.ID, &post.Title, &post.Body)
+	return post
+}
+
 // HomeHandler handles a route and template
-func HomeHandler (w http.ResponseWriter, r *http.Request) {
-	t :=  template.Must(template.ParseFiles("templates/index.html"))
-	if err := t.ExecuteTemplate(w, "index.html", listPosts()); err != nil {
+func HomeHandler(w http.ResponseWriter, r *http.Request) {
+	t := template.Must(template.ParseFiles("templates/index.html"))
+	if err := t.ExecuteTemplate(w, "index.html", GetPosts()); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+}
+
+// ViewHandler shows one post
+func ViewHandler(w http.ResponseWriter, r *http.Request) {
+	id := mux.Vars(r)["id"]
+	t := template.Must(template.ParseFiles("templates/view.html"))
+	if err := t.ExecuteTemplate(w, "view.html", GetPostById(id)); err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 	}
 }
